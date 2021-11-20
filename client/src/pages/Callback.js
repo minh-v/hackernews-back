e5392b04b28d1d50f416bec02546997f4d2c7fd4
Minh Vu <minhhv1@uci.edu>
2021-11-19 22:03:29 -0800

    temp removed proxy from package.json

--- client/src/pages/Callback.js
+++ client/src/pages/Callback.js
@@ -1,27 +1,27 @@
-import magic from "../magic"
-import { useHistory } from "react-router-dom"
+// import magic from "../magic"
+// import { useHistory } from "react-router-dom"
 
-const Callback = () => {
-  const authenticateWithServer = async (didToken) => {
-    let res = await fetch("http://localhost:3001/login", {
-      method: "POST",
-      credentials: "include",
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: "Bearer " + didToken,
-      },
-    })
-    history.push("/")
-  }
+// const Callback = () => {
+//   const authenticateWithServer = async (didToken) => {
+//     let res = await fetch("http://localhost:3001/login", {
+//       method: "POST",
+//       credentials: "include",
+//       headers: {
+//         "Content-Type": "application/json",
+//         Authorization: "Bearer " + didToken,
+//       },
+//     })
+//     history.push("/")
+//   }
 
-  const history = useHistory()
-  magic.auth.loginWithCredential().then((didToken) => authenticateWithServer(didToken))
-  console.log("push")
-  //close window
-  window.close()
-  //history.go("/")
+//   const history = useHistory()
+//   magic.auth.loginWithCredential().then((didToken) => authenticateWithServer(didToken))
+//   console.log("push")
+//   //close window
+//   window.close()
+//   //history.go("/")
 
-  return <div>loading..</div>
-}
+//   return <div>loading..</div>
+// }
 
-export default Callback
+// export default Callback
