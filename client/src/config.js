2161cf00464e8bb9aa04dcc84d0bcd1fef0db2f1
Minh Vu <minhhv1@uci.edu>
2021-11-19 21:54:29 -0800

    moved env variables to .env in client

--- client/src/config.js
+++ client/src/config.js
@@ -1,5 +1,7 @@
 const config = {
   MAGIC_API_KEY: "pk_live_35FBE7FE41B75731",
+  api: process.env.REACT_APP_API_URL,
+  hasura_url: process.env.REACT_APP_HASURA_ENDPOINT,
 }
 
 export default config
