5d166f4915c0805ad3aad16314b51f84446f866a
Minh Vu <minhhv1@uci.edu>
2021-11-21 23:25:26 -0800

    updated docker-compose

--- client/src/config.js
+++ client/src/config.js
@@ -1,7 +1,7 @@
 const config = {
   MAGIC_API_KEY: "pk_live_35FBE7FE41B75731",
-  api: process.env.NODE_ENV === "production" ? process.env.REACT_APP_API_URL : "http://localhost:3001",
-  hasura_endpoint: process.env.NODE_ENV === "production" ? process.env.REACT_APP_HASURA_ENDPOINT : "localhost:8080/v1/graphql",
+  api: process.env.REACT_DEBUG === "production" ? process.env.REACT_APP_API_URL : "http://localhost:3001",
+  hasura_endpoint: process.env.REACT_DEBUG === "production" ? process.env.REACT_APP_HASURA_ENDPOINT : "localhost:8080/v1/graphql",
 }
 
 export default config
