e38e9ed0bb5c119ef4ee2cb92507c99625bea8f5
Minh Vu <minhhv1@uci.edu>
2021-11-22 00:08:14 -0800

    deploy fix

--- client/src/config.js
+++ client/src/config.js
@@ -1,6 +1,6 @@
 const config = {
   MAGIC_API_KEY: "pk_live_35FBE7FE41B75731",
-  api: process.env.REACT_APP_DEBUG === "production" ? "http://hackernews-back.site/:3001" : "http://localhost:3001",
+  api: process.env.REACT_APP_DEBUG === "production" ? "http://hackernews-back.site:3001" : "http://localhost:3001",
   hasura_endpoint:
     process.env.REACT_APP_DEBUG === "production" ? "hackernews-back.site:8080/v1/graphql" : "localhost:8080/v1/graphql",
 }
--- server/server.js
+++ server/server.js
@@ -26,7 +26,7 @@ const { gql } = require("graphql-request")
 const app = express()
 const AGE = 60 * 60 * 24 * 7 * 10
 const corsOptions = {
-  origin: true,
+  origin: "*",
   credentials: true, //access-control-allow-credentials:true
   optionSuccessStatus: 200,
 }
